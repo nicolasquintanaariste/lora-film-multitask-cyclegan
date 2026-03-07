6a00aaed4999a7879c3a0fbb44a370f79f168c0c
Nicolas <quintann@tcd.ie>
2026-03-07 17:36:37 +0000

    Added MTT. Batch size is limited to task with smallest quantity of data. Added timer summary every epoch.

---
+++ Dissertation/pytorch-CycleGAN-and-pix2pix-film/data/film_dataset.py
@@ -0,0 +1,199 @@
+import os
+from data.base_dataset import BaseDataset, get_transform
+from data.image_folder import make_dataset
+from PIL import Image
+import random
+
+
+class FiLMDataset(BaseDataset):
+    """
+    This dataset class can load unaligned/unpaired datasets.
+
+    It requires two directories to host training images from domain A '/path/to/data/trainA'
+    and from domain B '/path/to/data/trainB' respectively.
+    You can train the model with the dataset flag '--dataroot /path/to/data'.
+    Similarly, you need to prepare two directories:
+    '/path/to/data/testA' and '/path/to/data/testB' during test time.
+    """
+
+    def __init__(self, opt, task):
+        """Initialize this dataset class.
+
+        Parameters:
+            opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
+        """
+        BaseDataset.__init__(self, opt)
+        self.dir_A = os.path.join(opt.dataroot_general, task, opt.phase + "A")  # create a path '/path/to/data/trainA'
+        self.dir_B = os.path.join(opt.dataroot_general, task, opt.phase + "B")  # create a path '/path/to/data/trainB'
+
+        self.A_paths = sorted(make_dataset(self.dir_A, opt.max_dataset_size))  # load images from '/path/to/data/trainA'
+        self.B_paths = sorted(make_dataset(self.dir_B, opt.max_dataset_size))  # load images from '/path/to/data/trainB'
+        self.A_size = len(self.A_paths)  # get the size of dataset A
+        self.B_size = len(self.B_paths)  # get the size of dataset B
+        btoA = self.opt.direction == "BtoA"
+        input_nc = self.opt.output_nc if btoA else self.opt.input_nc  # get the number of channels of input image
+        output_nc = self.opt.input_nc if btoA else self.opt.output_nc  # get the number of channels of output image
+        self.transform_A = get_transform(self.opt, grayscale=(input_nc == 1))
+        self.transform_B = get_transform(self.opt, grayscale=(output_nc == 1))
+
+    def __getitem__(self, index):
+        """Return a data point and its metadata information.
+
+        Parameters:
+            index (int)      -- a random integer for data indexing
+
+        Returns a dictionary that contains A, B, A_paths and B_paths
+            A (tensor)       -- an image in the input domain
+            B (tensor)       -- its corresponding image in the target domain
+            A_paths (str)    -- image paths
+            B_paths (str)    -- image paths
+        """
+        A_path = self.A_paths[index % self.A_size]  # make sure index is within then range
+        if self.opt.serial_batches:  # make sure index is within then range
+            index_B = index % self.B_size
+        else:  # randomize the index for domain B to avoid fixed pairs.
+            index_B = random.randint(0, self.B_size - 1)
+        B_path = self.B_paths[index_B]
+        A_img = Image.open(A_path).convert("RGB")
+        B_img = Image.open(B_path).convert("RGB")
+        # apply image transformation
+        A = self.transform_A(A_img)
+        B = self.transform_B(B_img)
+
+        return {"A": A, "B": B, "A_paths": A_path, "B_paths": B_path}
+
+    def __len__(self):
+        """Return the total number of images in the dataset.
+
+        As we have two datasets with potentially different number of images,
+        we take a maximum of
+        """
+        return max(self.A_size, self.B_size)
+
+
+
+
+# """Dataset class template
+
+# This module provides a template for users to implement custom datasets.
+# You can specify '--dataset_mode template' to use this dataset.
+# The class name should be consistent with both the filename and its dataset_mode option.
+# The filename should be <dataset_mode>_dataset.py
+# The class name should be <Dataset_mode>Dataset.py
+# You need to implement the following functions:
+#     -- <modify_commandline_options>:　Add dataset-specific options and rewrite default values for existing options.
+#     -- <__init__>: Initialize this dataset class.
+#     -- <__getitem__>: Return a data point and its metadata information.
+#     -- <__len__>: Return the number of images.
+# """
+
+# from data.base_dataset import BaseDataset, get_transform
+# import os
+
+# from data.image_folder import make_dataset
+# from PIL import Image
+# import random
+
+
+# class FiLMDataset(BaseDataset):
+#     """A template dataset class for you to implement custom datasets."""
+
+#     @staticmethod
+#     def modify_commandline_options(parser, is_train):
+#         """Add new dataset-specific options, and rewrite default values for existing options.
+
+#         Parameters:
+#             parser          -- original option parser
+#             is_train (bool) -- whether training phase or test phase. You can use this flag to add training-specific or test-specific options.
+
+#         Returns:
+#             the modified parser.
+#         """
+#         parser.add_argument("--new_dataset_option", type=float, default=1.0, help="new dataset option")
+#         parser.set_defaults(max_dataset_size=10, new_dataset_option=2.0)  # specify dataset-specific default values
+#         return parser
+
+#     def __init__(self, opt, tasks):
+#         """Initialize this dataset class.
+
+#         Parameters:
+#             opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
+
+#         A few things can be done here.
+#         - save the options (have been done in BaseDataset)
+#         - get image paths and meta information of the dataset.
+#         - define the image transformation.
+#         """
+#         # save the option and dataset root
+#         BaseDataset.__init__(self, opt)
+#         # get the image paths of your dataset;
+#         self.image_paths = []  # You can call sorted(make_dataset(self.root, opt.max_dataset_size)) to get all the image paths under the directory self.root
+#         # define the default transform function. You can use <base_dataset.get_transform>; You can also define your custom transform function
+#         self.transform = get_transform(opt)
+        
+#         self.taskId = random.randint(0, len(opt.tasks-1)) # Random interger to select the task performed for every batch
+#         task2id = {task: i for i, task in enumerate(opt.tasks)} if opt.tasks else None
+#         self.task_dataset = {}
+        
+#         for task in opt.tasks:
+#             dir_A = os.path.join(opt.dataroot_general, task, opt.phase + "A")  # create a path '/path/to/data/trainA'
+#             dir_B = os.path.join(opt.dataroot_general, task, opt.phase + "B")  # create a path '/path/to/data/trainB'
+
+#             A_paths = sorted(make_dataset(dir_A, opt.max_dataset_size))  # load images from '/path/to/data/trainA'
+#             B_paths = sorted(make_dataset(dir_B, opt.max_dataset_size))  # load images from '/path/to/data/trainB'
+#             A_size = len(A_paths)  # get the size of dataset A
+#             B_size = len(B_paths)  # get the size of dataset B
+#             btoA = self.opt.direction == "BtoA"
+#             input_nc = self.opt.output_nc if btoA else self.opt.input_nc  # get the number of channels of input image
+#             output_nc = self.opt.input_nc if btoA else self.opt.output_nc  # get the number of channels of output image
+#             transform_A = get_transform(self.opt, grayscale=(input_nc == 1))
+#             transform_B = get_transform(self.opt, grayscale=(output_nc == 1))
+#             tid = task2id[task]
+            
+#             self.tasks_dataset[tid] = {'dir_A':dir_A, 'dir_B':dir_B, 'A_paths':A_paths, 'B_paths':B_paths, 'A_size':A_size, 'B_size':B_size, 
+#                                       'btoA':btoA, 'input_nc':input_nc, 'output_nc':output_nc, 'transform_A':transform_A, 'transform_B':transform_B}
+            
+
+#     def __getitem__(self, index):
+#         """Return a data point and its metadata information.
+
+#         Parameters:
+#             index -- a random integer for data indexing
+
+#         Returns:
+#             a dictionary of data with their names. It usually contains the data itself and its metadata information.
+
+#         Step 1: get a random image path: e.g., path = self.image_paths[index]
+#         Step 2: load your data from the disk: e.g., image = Image.open(path).convert('RGB').
+#         Step 3: convert your data to a PyTorch tensor. You can use helpder functions such as self.transform. e.g., data = self.transform(image)
+#         Step 4: return a data point as a dictionary.
+#         """
+#         # path = "temp"  # needs to be a string
+#         # data_A = None  # needs to be a tensor
+#         # data_B = None  # needs to be a tensor
+#         # return {"data_A": data_A, "data_B": data_B, "path": path}
+    
+#         if self.taskId + 1 >= len(self.opt.tasks):
+#             self.taskId = 0
+#         else:
+#             taskId += 1
+#         dataset = self.get_task_dataset(taskId, index)
+    
+#     def get_task_dataset(self, tid, index):
+#         A_path = self.task_dataset[tid]['A_paths'][index % self.task_dataset[tid]['A_size']] # make sure index is within then range
+#         if self.opt.serial_batches:  # make sure index is within then range
+#             index_B = index % self.task_dataset[tid]['B_size']
+#         else:  # randomize the index for domain B to avoid fixed pairs.
+#             index_B = random.randint(0, self.task_dataset[tid]['B_size'] - 1)
+#         B_path = self.task_dataset[tid]['B_paths'][index_B]
+#         A_img = Image.open(A_path).convert("RGB")
+#         B_img = Image.open(B_path).convert("RGB")
+#         # apply image transformation
+#         A = self.task_dataset[tid]['transform_A'](A_img)
+#         B = self.task_dataset[tid]['transform_B'](B_img)
+
+#         return {"A": A, "B": B, "A_paths": A_path, "B_paths": B_path}
+
+#     def __len__(self):
+#         """Return the total number of images."""
+#         return len(self.image_paths)
---
+++ Dissertation/pytorch-CycleGAN-and-pix2pix-film/train.py
@@ -0,0 +1,213 @@
+"""General-purpose training script for image-to-image translation.
+
+This script works for various models (with option '--model': e.g., pix2pix, cyclegan, colorization) and
+different datasets (with option '--dataset_mode': e.g., aligned, unaligned, single, colorization).
+You need to specify the dataset ('--dataroot'), experiment name ('--name'), and model ('--model').
+
+It first creates model, dataset, and visualizer given the option.
+It then does standard network training. During the training, it also visualize/save the images, print/save the loss plot, and save models.
+The script supports continue/resume training. Use '--continue_train' to resume your previous training.
+
+Example:
+    Train a CycleGAN model:
+        python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan
+    Train a pix2pix model:
+        python train.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix --direction BtoA
+
+See options/base_options.py and options/train_options.py for more training options.
+See training and test tips at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/tips.md
+See frequently asked questions at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/qa.md
+"""
+
+import time
+from options.train_options import TrainOptions
+from data import create_dataset
+from models import create_model
+from util.visualizer import Visualizer
+from util.util import init_ddp, cleanup_ddp
+
+# Added from my version
+from utils import *
+from save_utils import *
+from metrics_utils import *
+from loss_utils import LossLogger, plot_losses
+
+
+if __name__ == "__main__":
+    opt = TrainOptions().parse()  # get training options
+    opt.device = init_ddp()
+    # dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+    
+    # Multitask datasets
+    if "film" in opt.netG.lower():
+        opt.dataset_mode = "film"
+    task2id = {t: i for i, t in enumerate(opt.tasks)}
+    task_datasets = {}
+    for task in opt.tasks:
+        task_datasets[task2id[task]] = create_dataset(opt, task) ### Change opt so that films makes use of FiLM datasets
+    max_dataset_size = max(len(dataset) for dataset in task_datasets.values())  # get the number of images in the dataset.
+    min_dataset_size = min(len(dataset) for dataset in task_datasets.values())
+    max_iters_per_epoch = min_dataset_size // opt.batch_size
+    print(f"The number of training images = {min_dataset_size}")
+    tid = random.randint(0, len(opt.tasks)-1)
+    
+    ####################################
+    # Added from my model
+    ####################################
+    image_folder = f"results/{opt.name}/images"
+    local_model_folder = f"results/{opt.name}"
+    loss_csv = f"{local_model_folder}/loss_log.csv"
+    loss_plot_path = f"{local_model_folder}/loss_plot.png"
+    logger = LossLogger(csv_path=loss_csv)
+    
+    start_time = datetime.datetime.now()
+    timer = PhaseTimer(use_cuda_sync=True)
+    transforms_ = [   # Image transformations
+        # transforms.Resize(int(opt.img_height * 1.12), Image.BICUBIC), # x1.12 would make img bigger and crop edges
+        transforms.Resize(256),  # Resize shortest side to img_height, maintains aspect ratio
+        transforms.RandomCrop((256, 256)),  # Now crop to square
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]
+    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.Tensor
+  
+    with timer.track("prep/fid_save_real"):
+        for task in opt.tasks:
+            real_dir = os.path.join(opt.dataroot_general, task, "testB_normalised")
+            fid_save_real(
+                in_dir=os.path.join(opt.dataroot_general, task),
+                out_dir=real_dir,
+                transforms_=transforms_,
+                max_images=250,
+                batch_size=opt.batch_size,
+            )
+    metric_logger = MetricLogger(csv_path=os.path.join(f"results/{opt.name}", "fid_kid.csv"))
+    
+    model = create_model(opt)  # create a model given opt.model and other options
+    model.setup(opt)  # regular setup: load and print networks; create schedulers
+    visualizer = Visualizer(opt)  # create a visualizer that display/save images and plots
+    total_iters = 0  # the total number of training iterations
+    for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):
+        with timer.track("train/epoch_total"):
+            epoch_start_time = time.time()  # timer for entire epoch
+            iter_data_time = time.time()  # timer for data loading per iteration
+            epoch_iter = 0  # the number of training iterations in current epoch, reset to 0 every epoch
+            visualizer.reset()
+            # Set epoch for DistributedSampler
+            for tid, dataset in task_datasets.items():
+                if hasattr(dataset, "set_epoch"):
+                    dataset.set_epoch(epoch)
+
+            # Choose one tid to train on per epoch
+            if tid+1 >= len(opt.tasks):
+                tid = 0
+            else: 
+                tid += 1 
+            
+            for i, data in enumerate(task_datasets[tid]):  # inner loop within one epoch
+                if i >= max_iters_per_epoch: # limit iterations to the smallest dataset
+                    break
+                iter_start_time = time.time()  # timer for computation per iteration
+                if total_iters % opt.print_freq == 0:
+                    t_data = iter_start_time - iter_data_time
+
+                total_iters += opt.batch_size
+                epoch_iter += opt.batch_size
+                model.set_input(data, tid)  # unpack data from dataset and apply preprocessing
+                model.optimize_parameters_film()  # calculate loss functions, get gradients, update network weights
+
+                if total_iters % opt.display_freq == 0:  # display images on visdom and save images to a HTML file
+                    save_result = total_iters % opt.update_html_freq == 0
+                    model.compute_visuals()
+                    visualizer.display_current_results(model.get_current_visuals(), epoch, total_iters, save_result)
+
+                if total_iters % opt.print_freq == 0:  # print training losses and save logging information to the disk
+                    losses = model.get_current_losses()
+                    t_comp = (time.time() - iter_start_time) / opt.batch_size
+                    visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
+                    visualizer.plot_current_losses(total_iters, losses)
+                    
+                    # Loggings added by me             
+                    logger.log(
+                        step=total_iters,
+                        loss_G=losses['G'],
+                        loss_GAN=losses['adversarial'],
+                        loss_cycle=losses['cycle'],
+                        loss_identity=losses['idt'],
+                        loss_D=losses['D'],
+                        loss_D_A=losses['D_A'],
+                        loss_D_B=losses['D_B'],
+                        dA_real_mean=model.D_A_real_mean,
+                        dA_fake_mean=model.D_A_fake_mean,
+                        dB_real_mean=model.D_B_real_mean,
+                        dB_fake_mean=model.D_B_fake_mean,
+                    )  
+
+                if total_iters % opt.save_latest_freq == 0:  # cache our latest model every <save_latest_freq> iterations
+                    print(f"saving the latest model (epoch {epoch}, total_iters {total_iters})")
+                    save_suffix = f"iter_{total_iters}" if opt.save_by_iter else "latest"
+                    model.save_networks(save_suffix)
+
+                iter_data_time = time.time()
+
+            model.update_learning_rate()  # update learning rates at the end of every epoch
+
+            if epoch % opt.save_epoch_freq == 0:  # cache our model every <save_epoch_freq> epochs
+                print(f"saving the model at the end of epoch {epoch}, iters {total_iters}")
+                model.save_networks("latest")
+                model.save_networks(epoch)
+                
+            # Plot FID
+            if (epoch-1) % 10 == 0: #opt.fid_interval == 0:
+                with timer.track("fid/compute"):
+                    for task in opt.tasks:
+                        fid_image_dir_A = f"results/{opt.name}/fake/{task}/A"
+                        fid_image_dir_B = f"results/{opt.name}/fake/{task}/B"                       
+                        fid_inference(epoch, opt, transforms_, task, task2id, Tensor, model.netG_A, model.netG_B, fid_image_dir_A, fid_image_dir_B)
+
+                        fake_dir = os.path.join(fid_image_dir_B, f"epoch{epoch:03d}")
+                        
+                        fid, kid = compute_fid_kid(real_dir, fake_dir)
+
+                        metric_logger.log(epoch=epoch, fid=fid, kid=kid, task=task)
+                        plot_fid(
+                            metric_logger,
+                            out_path=os.path.join(f"results/{opt.name}", f"fid.png"),
+                            show=False
+                        )
+                        plot_kid(
+                            metric_logger,
+                            out_path=os.path.join(f"results/{opt.name}", f"kid.png"),
+                            show=False
+                        )
+                    
+            # Generate samples and plot losses
+            if epoch % 1 == 0: #opt.sample_interval == 0:
+                with timer.track("sample_images/compute"):
+                    sample_images(epoch, opt, model.netG_A, model.netG_B, task2id, image_folder, Tensor)
+                    sample_images(epoch, opt, model.netG_A, model.netG_B, task2id, image_folder, Tensor, 42)
+                    plot_losses(logger, out_path=loss_plot_path, smooth_alpha=0.1, last_n=None, show=False)      
+            
+            # # Copy model folder to drive
+            # destination = os.path.join(base_folder, local_model_folder)
+            # copy_missing(session_model_folder, destination)
+                
+            
+
+            print(f"End of epoch {epoch} / {opt.n_epochs + opt.n_epochs_decay} \t Time Taken: {time.time() - epoch_start_time:.0f} sec")
+
+            # Save training summmary
+            end_time = datetime.datetime.now()
+            summary = {
+                "run_started_at": start_time.isoformat(timespec="seconds"),
+                "run_ended_at": end_time.isoformat(timespec="seconds"),
+                "total_seconds": (end_time - start_time).total_seconds(),
+                "tasks": opt.tasks,
+                # "lora": opt.lora is not None,
+                # "parameter_counts": param_summary,
+                "timings": timer.as_dict(),
+            }
+            save_run_summary(local_model_folder, summary)
+
+    cleanup_ddp()
